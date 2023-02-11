bd9c0f067e7611ddae2db673abc7e1aca66dcd1b
dyogaharshitha <dyogaharshitha@gmail.com>
2023-02-11 08:22:44 +0530

    web

--- dishes.py
+++ dishes.py
@@ -20,8 +20,43 @@ class dishesclass:
             df = dishdf[dishdf[col] == limit];
             return df;
 
-class defnutrireq:
+class nutrireq:
     def __init__(self):
-        #self.dishmethfrqreqdf = xldata.xltodf("static/database/dishmethfrqreq.xlsx");
-        #self.nutrireqdf= xldata.xltodf("static/database/nutrirew.xlsx");
+        self.dishmethfrqreqdf = pd.DataFrame();
+        self.nutrireqdf= pd.DataFrame();
+        self.fooditemfrq = pd.DataFrame();
+        self.dishtype = pd.DataFrame();
+        self.nutridf = pd.DataFrame();
+        self.foodclassdf = pd.DataFrame();
         print("dishes py");
+    def getdefaltnutrireq(self, df):
+        if self.dishmethfrqreqdf.equals(df):
+            {}# self.dishmethfrqreqdf = xldata.xltodf("static/database/dishmethfrqreq.xlsx");
+        if self.nutrireqdf.equals(df):
+            {}# self.nutrireqdf= xldata.xltodf("static/database/nutrirew.xlsx");
+        if self.fooditemfrq.equals(df):
+            {}#self.fooditemfrq = xldata.xltodf(r"C:\Users\Harshitha\Desktop\python\diet\database\foodnutri.xlsx");
+        if self.dishtype.equals(df):
+            {}# self.dishtype = xldata.xltodf(r"C:\Users\Harshitha\Desktop\python\diet\database\currytype.xlsx");
+        if self.nutridf.equals(df):
+            {}#self.nutridf = xldata.xltodf(r"C:\Users\Harshitha\Desktop\python\diet\database\nutrireq.xlsx");
+        if self.foodclassdf.equals(df):
+            {}#self.foodclassdf = xldata.xltodf(r"C:\Users\Harshitha\Desktop\python\diet\database\foodclass.xlsx");
+        else:
+            print("invalid");
+
+        return;
+
+class userdishdta:
+    def __init__(self):
+        self.usrdishusg= pd.DataFrame();
+    def getuserdishdta(self):
+        {}#self.foodclassdf = xldata.xltodf(r"C:\Users\Harshitha\Desktop\python\diet\database\raniusg.xlsx");
+    def logusrdta(self):
+        {}
+
+class glbalusrdta:
+    def __init__(self):
+        self.glblusrdtadf= pd.DataFrame();
+    def getglbalusrdta(self):
+        {}
--- test_updatedf.py
+++
@@ -1,2 +0,0 @@
-def test_srchkeywrd():
-    assert False
